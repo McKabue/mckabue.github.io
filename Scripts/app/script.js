50681be9750fef87946df44db65f87d1b7b7319e
Kabue <mckabue@gmail.com>
2015-09-10 14:46:12 -0700

    var showResults = function (data) {     var arr = $.map(data[1], function (item) {         return {             value: item[0]         };     });     console.log(arr);     return arr; };

--- Scripts/app/script.js
+++ Scripts/app/script.js
@@ -6,7 +6,15 @@
             var el = $(element);
             var options = ko.utils.unwrapObservable(valueAccessor());
             var local = [{ val: 'Alabama' }, { val: 'Alaska' }, { val: 'Arizona' }, { val: 'Arkansas' }, { val: 'California' }, { val: 'Colorado' }, { val: 'Connecticut' }, { val: 'Delaware' }, { val: 'Florida' }, { val: 'Georgia' }, { val: 'Hawaii' }, { val: 'Idaho' }, { val: 'Illinois' }, { val: 'Indiana' }, { val: 'Iowa' }, { val: 'Kansas' }, { val: 'Kentucky' }, { val: 'Louisiana' }, { val: 'Maine' }, { val: 'Maryland' }, { val: 'Massachusetts' }, { val: 'Michigan' }, { val: 'Minnesota' }, { val: 'Mississippi' }, { val: 'Missouri' }, { val: 'Montana' }, { val: 'Nebraska' }, { val: 'Nevada' }, { val: 'New Hampshire' }, { val: 'New Jersey' }, { val: 'New Mexico' }, { val: 'New York' }, { val: 'North Carolina' }, { val: 'North Dakota' }, { val: 'Ohio' }, { val: 'Oklahoma' }, { val: 'Oregon' }, { val: 'Pennsylvania' }, { val: 'Rhode Island' }, { val: 'South Carolina' }, { val: 'South Dakota' }, { val: 'Tennessee' }, { val: 'Texas' }, { val: 'Utah' }, { val: 'Vermont' }, { val: 'Virginia' }, { val: 'Washington' }, { val: 'West Virginia' }, { val: 'Wisconsin' }, { val: 'Wyoming' }]
-
+            var showResults = function (data) {
+                var arr = $.map(data[1], function (item) {
+                    return {
+                        value: item[0]
+                    };
+                });
+                console.log(arr);
+                return arr;
+            };
 
             el.attr("autocomplete", "off").typeahead({
                 hint: options.hint,
@@ -14,24 +22,33 @@
                 minLength: options.minLength
             }, {
                 name: 'Searches',
-                display: 'val',
+                display: 'value',
                 source: new Bloodhound({
-                    datumTokenizer: function (d) {
-                        return Bloodhound.tokenizers.whitespace(d.val);
+                    datumTokenizer: function (data) {
+                        return Bloodhound.tokenizers.whitespace(data.value);
                     },
 
                     queryTokenizer: Bloodhound.tokenizers.whitespace,
 
-                    local: local,
+                    //local: local,
 
                     /*
                     prefetch: 'http://localhost:1106/api/Search/searchTerm',
-
+                    
                     remote: {
-                        url: 'http://localhost:1106/api/Search/searchTerm3/%QUERY',
+                        url: 'https://clients1.google.com/complete/search?q=' + '%QUERY' + '&hl=en&client=partner&source=gcsc&partnerid=007448797502181840672:minf_ltsr2s=cse&nocache=' + Math.random().toString(),
                         wildcard: '%QUERY'
-                    }
+                    },
                     */
+                    remote: {
+                        url: "http://clients1.google.com/complete/search?client=partner&hl=en&sugexp=gsnos%2Cn%3D13&gs_rn=25&gs_ri=partner&partnerid=004914516364918182382%3Ayfqw09r4qvu&types=t&ds=cse&cp=3&gs_id=15&q=%QUERY",
+                        ajax: {
+                            type: 'GET',
+                            dataType: 'jsonp'
+                        },
+                        filter: showResults
+                    }
+                    
                 }),
                 templates: {
                     empty: [
