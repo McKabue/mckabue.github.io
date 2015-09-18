780f8d0fd640574ff53de4cd144d358a303a8ab7
Kabue <mckabue@gmail.com>
2015-09-18 16:26:18 -0700

    margin-top: 20px;

--- app/mvvm/social/social.js
+++ app/mvvm/social/social.js
@@ -18,6 +18,8 @@
             router: router,
 
             activate: function () {
+                stLight.options({ publisher: "c6eec58b-537b-4557-a746-b086dd18a9b8", doNotHash: false, doNotCopy: false, hashAddressBar: false });
+
                 disqus();
                
             },
